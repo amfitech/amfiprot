75a4efa2e4d57779945f71f8c18f4356083ef328
Kristian Klein-Wengel <kkw@amfitech.dk>
2022-08-08 16:00:32 +0200

    AM-294 Fix package requirements and versioning #done

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, Amfitech'
 author = 'Kristian Klein-Wengel'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = '0.0.1a0'
 
 
 # -- General configuration ---------------------------------------------------
