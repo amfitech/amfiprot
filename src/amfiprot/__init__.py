7cc1098cc6befbb65825505aa2d3f7962d5040f0
Daniel Siboska <ds@amfitech.dk>
2023-09-15 15:04:41 +0200

    Version 0.1.4

--- src/amfiprot/__init__.py
+++ src/amfiprot/__init__.py
@@ -1,5 +1,5 @@
 __all__ = ['connection', 'usb_connection', 'device', 'packet', 'payload', 'common_payload']
-__version__ = '0.1.3'
+__version__ = '0.1.4'
 
 from .payload import Payload, PayloadType
 from .node import Node
